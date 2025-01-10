31b13697cda6da44b120a1ded7617527b7df1311
daffadon <daffaputranarendra9@gmail.com>
2025-01-10 22:36:48 +0700

    feat(news): add get news by title

--- server/trpc/routers/news.ts
+++ server/trpc/routers/news.ts
@@ -1,8 +1,28 @@
 import { prisma } from "@/server/db";
 import { createTRPCRouter, publicProcedure } from "../variable";
+import z from "zod";
 
 export const newsRouter = createTRPCRouter({
-  getNews: publicProcedure.query(() => {
-    return prisma.news.findMany();
+  getNews: publicProcedure.query(async () => {
+    try {
+      const news = await prisma.news.findMany();
+      return news;
+    } catch (error) {
+      return null;
+    }
   }),
+  getNewsByTitle: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ input }) => {
+      try {
+        const news = await prisma.news.findFirst({
+          where: {
+            id: input.id,
+          },
+        });
+        return news;
+      } catch (error) {
+        return null;
+      }
+    }),
 });

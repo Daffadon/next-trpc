2a293df551a50ae8fcef091291a1812575140b63
daffadon <daffaputranarendra9@gmail.com>
2025-01-10 20:36:37 +0700

    refactor(UI): add universal styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import NewsList from "@/components/page/NewsList";
 import { serverClient } from "./_trpc/serverClient";
 
 export default async function Home() {
-  const news =  await serverClient.news.getNews();
+  const news = await serverClient.news.getNews();
   return (
     <>
-      <h1>News App</h1>
+      <h1 className="text-xl pt-4 text-center font-semibold">News App</h1>
       <NewsList news={news} />
     </>
   );

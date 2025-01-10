893575a7975c87b8a11a79e27cc14f3f30a6eb53
daffadon <daffaputranarendra9@gmail.com>
2025-01-10 20:37:22 +0700

    feat(UI): add NewsCard for home page

---
+++ components/card/NewsCard.tsx
@@ -0,0 +1,26 @@
+import { News } from "@prisma/client";
+
+const NewsCard = ({ news }: { news: News }) => {
+  return (
+    <div className="flex p-5 rounded-md z-10 relative">
+      <div>
+        <div className="flex items-center justify-between text-white">
+          <p className="text-sm text-yellow-500">
+            {news.publishedAt.toLocaleDateString()}
+          </p>
+          <p className="text-sm text-right underline">{news.source}</p>
+        </div>
+        <h6 className="text-md mt-2 w-full font-semibold text-white">
+          {news.title}
+        </h6>
+        <h6 className="text-sm w-full text-white">
+          Authored By{" "}
+          <span className="font-semibold text-yellow-400">{news.author}</span>
+        </h6>
+        <p className="text-sm w-full text-white mt-4">{news.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
--- components/page/NewsList.tsx
+++ components/page/NewsList.tsx
@@ -1,6 +1,10 @@
 "use client";
+
 import { trpc } from "@/app/_trpc/client";
 import { serverClient } from "@/app/_trpc/serverClient";
+import NewsCard from "../card/NewsCard";
+import Link from "next/link";
+
 const NewsList = ({
   news,
 }: {
@@ -12,13 +16,26 @@ const NewsList = ({
     refetchOnReconnect: false,
   });
   return (
-    <div>
-      <h1>list of news</h1>
-      <ul>
-        {getNews.data?.map((news) => (
-          <li key={news.id}>{news.title}</li>
-        ))}
-      </ul>
+    <div className="mt-10">
+      <div className="flex gap-x-4 gap-y-2 justify-center flex-wrap ">
+        {getNews.data ? (
+          getNews.data?.map((news) => (
+            <Link
+              href={`/news/${news.id}`}
+              key={news.id}
+              className="relative max-w-[25em] w-[20em] sm:w-[22em] md:w-[25em] bg-cover bg-center bg-no-repeat p-5 shadow-lg rounded-md cursor-pointer hover:scale-[101%] duration-100"
+              style={{ backgroundImage: `url(${news.image})` }}
+            >
+              <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-75 rounded-md z-10"></div>
+              <div className="relative z-20">
+                <NewsCard news={news} />
+              </div>
+            </Link>
+          ))
+        ) : (
+          <div className="text-center font-semibold text-lg">No news found</div>
+        )}
+      </div>
     </div>
   );
 };

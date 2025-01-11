b8b613c31e888ad7e5c7a501c5c93a3d6c4454d3
daffadon <daffaputranarendra9@gmail.com>
2025-01-11 13:01:33 +0700

    refactor(protocol): modularize make query client

--- app/_trpc/client.ts
+++ app/_trpc/client.ts
@@ -1,5 +1,29 @@
 import { createTRPCReact } from "@trpc/react-query";
+import {
+  defaultShouldDehydrateQuery,
+  QueryClient,
+} from "@tanstack/react-query";
+import superjson from "superjson";
 
 import type { AppRouter } from "@/server/trpc";
 
-export const trpc = createTRPCReact<AppRouter>({});
+export const trpcClient = createTRPCReact<AppRouter>({});
+
+export function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 30 * 1000,
+      },
+      dehydrate: {
+        serializeData: superjson.serialize,
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === "pending",
+      },
+      hydrate: {
+        deserializeData: superjson.deserialize,
+      },
+    },
+  });
+}

6dba68995e5c14497d91b5b0b41790336c9fb88f
daffadon <daffaputranarendra9@gmail.com>
2025-01-11 13:03:15 +0700

    refactor(trcp): add dummy context

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,14 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { appRouter } from "@/server/trpc";
+import { createTRPCContext } from "@/server/trpc/variable";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: createTRPCContext,
   });
 
 export { handler as GET, handler as POST, handler as PUT, handler as DELETE };

e5f30b2219fd2454ae1021c840a5d2576d6012e4
daffadon <daffaputranarendra9@gmail.com>
2025-02-06 12:22:22 +0700

    refactor(UI): implement navbar and addarticlebutton

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import NewsList from "@/components/page/NewsList";
+import NewsList from "@/components/page/home/NewsList";
 import { HydrateClient, trpc } from "./_trpc/serverClient";
+import AddArticleButton from "@/components/page/home/AddArticleButton";
+import Navbar from "@/components/appbar/Navbar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   const news = await trpc.news.getNews();
   return (
     <HydrateClient>
-      <h1 className="text-xl pt-4 text-center font-semibold">News App</h1>
+      <Navbar />
       <NewsList news={news} />
+      <AddArticleButton />
     </HydrateClient>
   );
 }

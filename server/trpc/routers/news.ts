9af7507247010d9bb1c8bfa0e126200e865dc0c4
daffadon <daffaputranarendra9@gmail.com>
2025-02-06 12:30:28 +0700

    feat(news): add addnews feature

--- server/trpc/routers/news.ts
+++ server/trpc/routers/news.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/server/db";
 import { createTRPCRouter, publicProcedure } from "../variable";
 import z from "zod";
+import { addArticleSchema } from "@/validation/form";
+import { TRPCError } from "@trpc/server";
 
 export const newsRouter = createTRPCRouter({
   getNews: publicProcedure.query(async () => {
@@ -11,7 +13,7 @@ export const newsRouter = createTRPCRouter({
       return null;
     }
   }),
-  getNewsByTitle: publicProcedure
+  getNewsById: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(async ({ input }) => {
       try {
@@ -23,6 +25,40 @@ export const newsRouter = createTRPCRouter({
         return news;
       } catch (error) {
         return null;
+      }
+    }),
+  addNews: publicProcedure
+    .input(addArticleSchema)
+    .mutation(async ({ input }) => {
+      if (!input || typeof input !== "object") {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Received null or invalid input",
+        });
+      }
+      try {
+        const news = await prisma.news
+          .create({
+            data: {
+              ...input,
+              publishedAt: new Date(),
+            },
+          })
+          .catch((err) => {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: "Database issue",
+            });
+          });
+        return input;
+      } catch (error: any) {
+        if (error instanceof TRPCError) {
+          return error;
+        }
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something wrong with db transaction",
+        });
       }
     }),
 });
